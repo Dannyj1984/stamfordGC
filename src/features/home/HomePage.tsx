970f5954394c4f01adcc66494c1166b09dbc7ec8
Danny Jebb <dannyjebb@gmail.com>
2022-11-08 17:07:03 +0000

    first commit

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -69,11 +69,11 @@ export default function HomePage() {
                         <Typography variant="h3" color='text.secondary' sx={{justifyContent: 'center', paddingBottom:'10px'}}>Welcome to Stamford Golf Club</Typography>
                     </div>
                         <Typography variant='h6' color='text.secondary' sx={{ paddingBottom:'10px'}}>
-                            We are glad to welcome back members to Stamford golf club. We are also welcoming visitors and ask you to contact to professional's shop on 01457 832126 to book.
+                            We are glad to welcome back members to Stamford golf club. We are also welcoming visitors and ask you to see below to book or contact to professional's shop on <a href='tel:01457 832126'>01457 832126</a> to book.
                         </Typography>
 
                         <Typography variant='h6' color='text.secondary' sx={{ paddingTop:'20px'}}>
-                            For general enquiries please contact the Admin Manager on 01457 832126 email admin@stamfordgolfclub.co.uk
+                            For general enquiries please contact the Admin Manager on <a href='tel:01457 832126'>01457 832126</a> email <a href="mailto:admin@stamfordgolfclub.co.uk">admin@stamfordgolfclub.co.uk</a>
                         </Typography>
                     
                         
