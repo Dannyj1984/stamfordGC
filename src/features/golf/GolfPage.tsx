476e81b762d9aaf2d725d4c52eaeb164d1c96608
Danny Jebb <dannyjebb@gmail.com>
2022-11-10 12:50:46 +0000

    membership table done

--- src/features/golf/GolfPage.tsx
+++ src/features/golf/GolfPage.tsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 import { useState } from "react";
 import { Container } from "@mui/system";
+import MembershipTable from "./MembershipTable";
 
 const navItems = [
     {title: 'Green Fees', path: '/fees'},
@@ -38,6 +39,12 @@ export default function GolfPage() {
     const showLinksFunc = () => {
         setShowLinks(!showLinks)
     }
+
+    const date = new Date();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const day = date.getDate();
+
     return (
         <>
         <Box component='img' style={imgStyle} sx={{height:'40vh', width:'100%', objectFit: 'fill'}}>
@@ -93,12 +100,71 @@ export default function GolfPage() {
             </Container>
         </Box>   
         }
-        <Paper elevation={4} sx={{height: '85vh'}}>
-            
+        <Paper elevation={4} sx={{ height: '30vh', display:'flex', flexDirection:'column', justifyContent:'flex-start', alignItems:'center', paddingTop:'3rem'}}>
+            <img src="/img/stamford.webp" alt="logo" width={80} height={80}/>
+            <Typography variant='h5'>We offer a warm welcome to visiting parties of any size</Typography>
         </Paper>
         <Divider />
-        <Paper elevation={4} sx={{height: '85vh'}}>
+        <Paper  elevation={4} sx={{backgroundColor:'#ebebeb', height: '85vh', display:'flex', flexDirection:'column', justifyContent:'flex-start', alignItems:'center'}}>
+            <Typography variant='h4'>Green Fees</Typography>
+            <Paper elevation={4} sx={{width: '90%', marginTop: '10px', padding:'10px'}}>
+                <Box sx={{display:'flex', flexDirection:'column', justifyContent:'flex-start', backgroundColor:'white', alignItems:'center'}}>
+                    <Typography variant="h6">Winter 2022/23 Green Fees</Typography>
+                    <Typography sx={{fontSize:'1rem'}}>1st Nov - 31st March</Typography>
+                </Box>
+                <Divider />
+                <Box sx={{backgroundColor:'white'}}>
+                    <Typography >Monday - £20 per round (Clubhouse not open)</Typography>
+                    <Typography >Tuesday - Friday £29 per round</Typography>
+                    <Typography >Saturday - £30 per round (After 1:30pm)</Typography>
+                    <Typography >Sunday - £30 per round (After 10am)</Typography>
+                </Box>
+            </Paper>
+
+            <Paper sx={{width: '90%', marginTop: '10px', padding:'10px'}}>
+                <Box sx={{display:'flex', flexDirection:'column', justifyContent:'flex-start', backgroundColor:'white', alignItems:'center'}}>
+                    <Typography variant="h6">Summer 2023 Green Fees</Typography>
+                </Box>
+                <Divider />
+                <Box sx={{backgroundColor:'white'}}>
+                    <Typography >Monday - £25 per round (Clubhouse not open)</Typography>
+                    <Typography >Tuesday - Friday £35 per round</Typography>
+                    <Typography >Saturday - £40 per round (After 1:30pm)</Typography>
+                    <Typography >Sunday - £40 per round (After 10am)</Typography>
+                </Box>
+            </Paper>
+            <ListItem 
+                sx={{justifyContent:'center'}}
+                component={NavLink} 
+                to={{pathname: `https://stamford.hub.clubv1.com/visitors/TeeSheet?date=${year}-${month+1}-${day}`}} target="_blank">
+                    <Button variant="contained">Online Booking</Button>
+                </ListItem>
             
+        </Paper>
+        <Paper  elevation={4} sx={{backgroundColor:'#c8d8ce',  display:'flex', flexDirection:'column', justifyContent:'flex-start', alignItems:'center', paddingBottom: '20px'}}>
+            <Typography variant='h4'>Membership</Typography>
+
+            <Paper sx={{width: '90%', marginTop: '10px', padding:'10px'}}>
+                <Typography variant="h6">Benefits:</Typography>
+                <Typography>We run a full program of competitions. Our main competition day is Saturday which has a full summer program including medal rounds and our Majors. We also have competitions on Sunday morning, as well as a weekly Wednesday competition throughout the summer. Our competition start sheet allows new members to quickly meet others. Tee bookings are made in 30 minute time slots, with a draw being made to to decide on the groups.</Typography>
+                <br />
+                <Typography>We have a strong seniors section with dedicated competitions times twice a week and we have a Tuesday morning roll up which is open to all members.</Typography>
+                <br />
+                <Typography>Our lady members also have competitions during the week.</Typography>
+                <br />
+                <Typography>We have a friendly membership and our pro Mark will be happy to assist in finding you a pairing.</Typography>
+                <br />
+                <Typography>From January 2023, Stamford Golf Club share a Reciprocal agreement for our members at other courses. Including Chorlton, Disley and Bury Conditions apply* <br />
+We are encouraging participation in golf for people of all ages and abilities. We have membership packages that are designed to suite your needs.</Typography>
+                <br />
+                <Box sx={{display:'flex', flexDirection:'column', justifyContent:'flex-start', backgroundColor:'white', alignItems:'center'}}>
+                    <Typography variant="h5">Membership Fees 2023</Typography>
+                    <Typography variant="body1">Membership runs from 1st April - 31st March each year</Typography>
+
+                </Box>
+                <Divider />
+                <MembershipTable />
+            </Paper>
         </Paper>
         </>
     )
---
+++ src/features/golf/MembershipTable.tsx
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Divider } from '@mui/material';
+
+function createData(
+  category: string,
+  year1Cost: number,
+  year2Cost: number,
+) {
+  return { category, year1Cost, year2Cost };
+}
+
+const rowsMale = [
+  createData('31 years and above', 850, 850),
+  createData('26-30 years', 449, 449),
+  createData('24-25 years', 333, 333),
+  createData('22-23 years', 269, 269),
+  createData('20-21 years', 206, 206),
+  createData('6 day member excl Saturday', 740, 740),
+  createData('Midweek member Mon - Fri', 634, 634),
+  createData('3 day Member (Mon - Wed- Sun)', 520, 520)
+];
+
+const rowsFemale = [
+    createData('31 years and above', 493, 493),
+    createData('26-30 years', 260, 260),
+    createData('24-25 years', 193, 193),
+    createData('22-23 years', 156, 156),
+    createData('20-21 years', 119, 119)
+  ];
+
+  const rowsJunior = [
+    createData('Youth member - 19 years', 100, 100),
+    createData('Youth member - 18 years', 90, 90),
+    createData('Youth member - 15-17 years', 75, 75)
+  ];
+
+  const rowsOther = [
+    createData('Academy membership (see professional for info', 312.50, 312.50),
+    createData('Social', 25, 25)
+  ];
+
+export default function MembershipTable() {
+  return (
+    <>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 300 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Membership Category (Male)</TableCell>
+            <TableCell align="right">2022</TableCell>
+            <TableCell align="right">2023</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rowsMale.map((row) => (
+            <TableRow
+              key={row.category}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.category}
+              </TableCell>
+              <TableCell align="right">£{row.year1Cost}</TableCell>
+              <TableCell align="right">£{row.year2Cost}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+<Divider />
+<br />
+<TableContainer component={Paper}>
+<Table sx={{ minWidth: 300 }} aria-label="simple table">
+  <TableHead>
+    <TableRow>
+      <TableCell>Membership Category (Female)</TableCell>
+      <TableCell align="right">2022</TableCell>
+      <TableCell align="right">2023</TableCell>
+    </TableRow>
+  </TableHead>
+  <TableBody>
+    {rowsFemale.map((row) => (
+      <TableRow
+        key={row.category}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {row.category}
+        </TableCell>
+        <TableCell align="right">£{row.year1Cost}</TableCell>
+        <TableCell align="right">£{row.year2Cost}</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+</TableContainer>
+<Divider />
+<br />
+<TableContainer component={Paper}>
+<Table sx={{ minWidth: 300 }} aria-label="simple table">
+  <TableHead>
+    <TableRow>
+      <TableCell>Membership Category (Junior)</TableCell>
+      <TableCell align="right">2022</TableCell>
+      <TableCell align="right">2023</TableCell>
+    </TableRow>
+  </TableHead>
+  <TableBody>
+    {rowsJunior.map((row) => (
+      <TableRow
+        key={row.category}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {row.category}
+        </TableCell>
+        <TableCell align="right">£{row.year1Cost}</TableCell>
+        <TableCell align="right">£{row.year2Cost}</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+</TableContainer>
+<Divider />
+<br />
+<TableContainer component={Paper}>
+<Table sx={{ minWidth: 300 }} aria-label="simple table">
+  <TableHead>
+    <TableRow>
+      <TableCell>Membership Category (Other)</TableCell>
+      <TableCell align="right">2022</TableCell>
+      <TableCell align="right">2023</TableCell>
+    </TableRow>
+  </TableHead>
+  <TableBody>
+    {rowsOther.map((row) => (
+      <TableRow
+        key={row.category}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {row.category}
+        </TableCell>
+        <TableCell align="right">£{row.year1Cost}</TableCell>
+        <TableCell align="right">£{row.year2Cost}</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+</TableContainer>
+</>
+  );
+}
